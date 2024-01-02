77bea5e18a7993d9d5bb255457e171e46d677045
ojiwon99 <dhwldnjs0420@naver.com>
2024-01-02 16:23:59 +0900

    20240102

--- src/com/javaex/Practice/Ex02.java
+++ src/com/javaex/Practice/Ex02.java
@@ -3,8 +3,8 @@ package com.javaex.Practice;
 public class Ex02 {
 
 	public static void main(String[] args) {
-		/*[문제] Ex02 double 형 배열 을 작성하고 1.2, 3.3, 6.7 값을 순서대로 입력하고 아래와 같이 출력되도록 for문
-        을 작성하세요.*/
+		/*[문제] Ex02 double 형 배열 을 작성하고 1.2, 3.3, 6.7 값을 
+		 순서대로 입력하고 아래와 같이 출력되도록 for문을 작성하세요.*/
     
     double[] dArray= new double[3];
     
